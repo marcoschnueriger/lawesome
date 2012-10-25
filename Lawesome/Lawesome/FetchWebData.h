746a63df5f3ac4af49cabacce6a9e06abdd3df59
Marco Schnüriger <anni@Macintosh-3.local>
2012-10-25 10:29:20 +0200

    Added FetchWebData Class

---
+++ Lawesome/Lawesome/FetchWebData.h
@@ -0,0 +1,13 @@
+//
+//  FetchWebData.h
+//  Lawesome
+//
+//  Created by Marco Schnüriger on 25.10.12.
+//  Copyright (c) 2012 2am Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FetchWebData : NSObject
+
+@end
---
+++ Lawesome/Lawesome/FetchWebData.m
@@ -0,0 +1,13 @@
+//
+//  FetchWebData.m
+//  Lawesome
+//
+//  Created by Marco Schnüriger on 25.10.12.
+//  Copyright (c) 2012 2am Development. All rights reserved.
+//
+
+#import "FetchWebData.h"
+
+@implementation FetchWebData
+
+@end
